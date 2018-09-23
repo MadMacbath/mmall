2557c5eadf0945bd15a9ef106225319d0a53db16
madmacbath <madmacbath@163.com>
2018-09-23 15:12:14 +0800

    m

---
+++ m-mall/src/main/java/com/macbeth/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.macbeth.dao;
+
+import com.macbeth.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
